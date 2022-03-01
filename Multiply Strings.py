c523a83b1c796aa2f472e34acb90ad508479744d
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-03-01 19:17:32 +0530

    Add files via upload

---
+++ Multiply Strings.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1)*int(num2))
