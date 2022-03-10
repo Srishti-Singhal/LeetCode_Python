bedb45d2ef327ed3b921ebf9ca3e36d2bbb0f392
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-03-10 19:45:52 +0530

    Add files via upload

---
+++ Longest common prefix.py
@@ -0,0 +1,9 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+ result=""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return result
+            result += strs[0][i]
+        return result
