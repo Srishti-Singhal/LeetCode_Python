b810edc935ee5b692a912fdbd5c832521f31621d
Srishti Singhal <99279286+Srishti-Singhal@users.noreply.github.com>
2022-04-09 12:03:03 +0530

    Add files via upload

---
+++ Merge sorted array.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        del nums1[m:]
+        nums1.extend(nums2)
+        nums1.sort()
+        
